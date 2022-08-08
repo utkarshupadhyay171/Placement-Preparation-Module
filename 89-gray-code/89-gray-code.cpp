e853bc79132adf35d28842912cfa6c81bbafdba7
Utkarsh Upadhyay <71688090+utkarshupadhyay171@users.noreply.github.com>
2022-08-08 18:10:52 +0530

    Time: 103 ms (10.33%), Space: 39.9 MB (9.35%) - LeetHub

---
+++ 89-gray-code/89-gray-code.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) 
+    {
+        vector<string>temp=help(n);
+        vector<int>ans;
+        for(auto i:temp)
+            ans.push_back(stoi(i,0,2));
+        return ans;
+        
+        
+        
+        return ans;
+    }
+    
+    vector<string>help(int n)
+    {
+        if(n==1)
+            return {"0","1"};
+        vector<string>ans;
+        vector<string>temp=help(n-1);
+        for(int i=0;i<temp.size();i++)
+            ans.push_back("0"+temp[i]);
+        for(int i=temp.size()-1;i>=0;i--)
+            ans.push_back("1"+temp[i]);
+        
+        return ans;
+        
+    }
+};
