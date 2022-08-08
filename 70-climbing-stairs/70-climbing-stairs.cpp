43c178777a94c117e18b97898e8e1d0ed5eaf30b
Utkarsh Upadhyay <71688090+utkarshupadhyay171@users.noreply.github.com>
2022-08-08 17:29:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.67%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n)
+    {
+        int a[n+1];
+        a[0]=1;
+        a[1]=1;
+        for(int i=2;i<n+1;i++)
+            a[i]=a[i-1]+a[i-2];
+        
+        return a[n];
+        
+    }
+};
