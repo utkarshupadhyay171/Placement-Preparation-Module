350885eb07a32016311083a350ae6907687fb408
Utkarsh Upadhyay <71688090+utkarshupadhyay171@users.noreply.github.com>
2022-08-09 21:55:40 +0530

    Time: 97 ms (50.42%), Space: 9.6 MB (76.64%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool asa(string&s,int n)
+    {
+        for(int i=1;i<=n;i++)
+            if(s[i-1]!='*')
+                return false;
+        return true;
+    }
+    
+  
+    
+    bool isMatch(string s, string p) 
+    {
+        int m=s.size(),n=p.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        dp[0][0]=true;
+        for(int j=1;j<=m;j++) dp[0][j]=false;
+        for(int i=1;i<=n;i++)
+        {
+            dp[i][0]=asa(p,i);
+            
+        }
+     
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(p[i-1]==s[j-1]||p[i-1]=='?')
+                    dp[i][j]=dp[i-1][j-1];
+                else if(p[i-1]=='*')
+                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                else
+                    dp[i][j]=false;
+            }
+        }
+        return dp[n][m];
+        
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
