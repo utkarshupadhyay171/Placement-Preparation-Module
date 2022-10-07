2da51f62fa916ef56a58d72513733563d36fe646
Utkarsh Upadhyay <71688090+utkarshupadhyay171@users.noreply.github.com>
2022-10-07 17:24:03 +0530

    Time: 19 ms (5.03%), Space: 7.7 MB (10.71%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -3,22 +3,17 @@ public:
     int titleToNumber(string s) 
     {
         unordered_map<char,int>map;
-        for(int i=0;i<26;i++)
+        for(int i=1;i<=26;i++)
         {
-            char c=i+65;
-            map[c]=i+1;
+            map['A'+i-1]=i;
         }
-        
         int sum=0;
         long long x=1;
-        
         for(int i=s.size()-1;i>=0;i--)
         {
             sum+=map[s[i]]*x;
-            x*=26;
+            x=x*26;
         }
-        
         return sum;
-        
     }
 };
